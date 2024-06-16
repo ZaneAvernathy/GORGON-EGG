7b0d4714fc9c684c902c6e0b1b8fdc5b00dddfb9
ZaneAvernathy <zavernathy@gmail.com>
2024-06-16 06:38:59 -0400

    Add Ring module

--- template/CommonDefinitions.h
+++ template/CommonDefinitions.h
@@ -155,6 +155,44 @@
   #define FID_FACTION_CHIBI 0x7F00 // Decomp's name
   #define IS_GENERIC_MINIMUG(portraitID) (portraitID >= FID_FACTION_CHIBI)
 
+  #define OAM0_Y(ay)         ((ay) & 0x00FF)
+  #define OAM0_AFFINE_ENABLE 0x0100
+  #define OAM0_DOUBLESIZE    0x0200
+  #define OAM0_DISABLE       0x0200
+  #define OAM0_BLEND         0x0400
+  #define OAM0_WINDOW        0x0800
+  #define OAM0_MOSAIC        0x1000
+  #define OAM0_256COLORS     0x2000
+  #define OAM0_SHAPE_8x8     0x0000
+  #define OAM0_SHAPE_16x16   0x0000
+  #define OAM0_SHAPE_32x32   0x0000
+  #define OAM0_SHAPE_64x64   0x0000
+  #define OAM0_SHAPE_16x8    0x4000
+  #define OAM0_SHAPE_32x8    0x4000
+  #define OAM0_SHAPE_32x16   0x4000
+  #define OAM0_SHAPE_64x32   0x4000
+  #define OAM0_SHAPE_8x16    0x8000
+  #define OAM0_SHAPE_8x32    0x8000
+  #define OAM0_SHAPE_16x32   0x8000
+  #define OAM0_SHAPE_32x64   0x8000
+
+  #define OAM1_X(ax)         ((ax) & 0x01FF)
+  #define OAM1_AFFINE_ID(ai) (((ai) << 9) & 0x3E00)
+  #define OAM1_HFLIP         0x1000
+  #define OAM1_VFLIP         0x2000
+  #define OAM1_SIZE_8x8      0x0000
+  #define OAM1_SIZE_16x8     0x0000
+  #define OAM1_SIZE_8x16     0x0000
+  #define OAM1_SIZE_16x16    0x4000
+  #define OAM1_SIZE_32x8     0x4000
+  #define OAM1_SIZE_8x32     0x4000
+  #define OAM1_SIZE_32x32    0x8000
+  #define OAM1_SIZE_32x16    0x8000
+  #define OAM1_SIZE_16x32    0x8000
+  #define OAM1_SIZE_64x64    0xC000
+  #define OAM1_SIZE_64x32    0xC000
+  #define OAM1_SIZE_32x64    0xC000
+
   #define OAM2_CHR(ac)   ((ac) & 0x3FF)
   #define OAM2_LAYER(al) (((al) & 0x3) << 10)
   #define OAM2_PAL(ap)   (((ap) & 0xF) << 12)
